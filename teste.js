9507dfa1645af6296012114c4a8c1dc2e88dfb56
sdlima4025 <sergiolimafg3@gmail.com>
2024-07-17 09:59:18 -0300

    estudo de casos de uso do console.log|.table|.time|.group|.dir

--- teste.js
+++ teste.js
@@ -1,35 +1,34 @@
 // jogo das frutas
-console.log('Bem vindo ao Jogo das Frutas!');
+console.log("Bem vindo ao Jogo das Frutas!");
 
 // Função que escolhe uma fruta aleatória
 function escolherFruta() {
-const frutas = ['Maçã', 'Laranja', 'Bananas', 'Uvas', 'kiwis'];
-const frutaEscolhida = frutas[Math.floor(Math.random() * frutas.length)];
-console.log('Fruta Escolhida :', frutaEscolhida);
-return frutaEscolhida;
+  const frutas = ["Maçã", "Laranja", "Bananas", "Uvas", "kiwis"];
+  const frutaEscolhida = frutas[Math.floor(Math.random() * frutas.length)];
+  console.log("Fruta Escolhida :", frutaEscolhida);
+  return frutaEscolhida;
 }
 
 function jogarJogo() {
-  console.group('Jogo das Frutas');
-  console.time('Tempo de Processamento');
+  console.group("Jogo das Frutas");
+  console.time("Tempo de Processamento");
   const frutasEscolhidas = [];
 
-  for(let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     console.group(`Rodada ${i + 1}`);
     const fruta = escolherFruta();
     frutasEscolhidas.push(fruta);
-    console.trace('Ponto de rastreamento para depuração');
+    console.trace("Ponto de rastreamento para depuração");
     console.groupEnd();
   }
   console.groupEnd();
 
-  console.log('Frutas escolhidas nas rodadas');
+  console.log("Frutas escolhidas nas rodadas");
   console.table(frutasEscolhidas);
-  console.timeEnd('Tempo de Processamento');
+  console.timeEnd("Tempo de Processamento");
   // inspecionando a lista de frutas
   console.dir(escolherFruta);
 }
-
 
 // iniciando o jogo
 jogarJogo();
